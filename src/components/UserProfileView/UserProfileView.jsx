8bff094585ade8e7cdb9fd49e4ef9e42b3ce64f1
Jonathan-Xue <jonathanxue50@gmail.com>
2019-05-03 06:16:25 -0500

    Untracked File

--- src/components/UserProfileView/UserProfileView.jsx
+++ src/components/UserProfileView/UserProfileView.jsx
@@ -1,17 +1,12 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Button } from "semantic-ui-react";
 
 import firebase from "../Firebase";
 import styles from "./UserProfileView.module.scss";
 import { getUser } from "../../backend_helper";
+import background from "./profile_pic.jpg";
 
-import {
-  FaBed,
-  FaBath,
-  FaHeart,
-  FaCalendarAlt,
-  FaUserAlt
-} from "react-icons/fa";
 import Navbar from "../Navbar/Navbar";
 
 class UserProfileView extends Component {
@@ -21,6 +16,8 @@ class UserProfileView extends Component {
     this.state = {
       user: {}
     };
+
+    this.addApartmentClickHandler.bind(this);
   }
 
   componentDidMount() {
@@ -36,12 +33,50 @@ class UserProfileView extends Component {
     });
   }
 
+  addApartmentClickHandler(event) {
+    console.log("Add Apartment");
+  }
+
   render() {
+    console.log(this.state.user);
     return (
       <div className={styles.userprofile}>
         <Navbar />
-        <h1>{this.state.user.name}</h1>
-      </div>
+        <div className={styles.profile}>
+          <img className={styles.profilePicture} src={background}></img>
+
+          <div className={styles.profileName}>
+            <h1>{this.state.user.Name}</h1>
+          </div>
+
+          <div className={styles.profileDetails}>
+            <span className={styles.profileDetailsSection}>
+              <p>Email: </p>
+              <p>{this.state.user.Email}</p>
+            </span>
+            
+            <span className={styles.profileDetailsSection}>
+              <p>Phone Number: </p>
+              <p>{this.state.user.CellPhone}</p>
+            </span>
+          </div>
+
+          <div className={styles.profileMisc}>
+            <Button fluid color='red' onClick={this.addApartmentClickHandler}>Add Apartment</Button>
+          </div>
+          
+        </div>
+
+        <div className={styles.apartments}>
+          <div className={styles.ownedApartments}>
+            <h1>My Apartments</h1>
+          </div>
+
+          <div className={styles.favoriteApartments}>
+            <h1>Favorite Apartments</h1>
+          </div>
+        </div>
+    </div>
     );
   }
 }
